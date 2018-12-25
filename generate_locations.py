1976f9cdd40be245e4ae9fa4a5c13ec881dacbde
Jake Coppinger <jake@jakecoppinger.com>
2018-12-26 00:11:53 +1100

    Initial commit

---
+++ generate_locations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import shapefile
+import json
+
+sf = shapefile.Reader("IDR00007.dbf")
+
+# Exclude the first item (deletionFlag)
+fields = [d[0] for d in sf.fields][1:]
+records = sf.records()
+
+locations = []
+
+for record in records:
+    location = {}
+    for i in range(len(fields)):
+        key = fields[i]
+        value = record[i]
+        location[key] = value
+    locations.append(location)
+
+# Pretty print JSON
+s = json.dumps(locations, sort_keys=True, indent=2)
+print(s)
